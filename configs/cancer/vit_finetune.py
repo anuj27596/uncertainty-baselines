9099347c8f4004f42e3153c6b693cc634f406d12
karm-patel <karmpatel216@gmail.com>
2023-07-17 15:13:09 +0530

    Copy pasted from chest to cancer

---
+++ configs/cancer/vit_finetune.py
@@ -0,0 +1,150 @@
+# coding=utf-8
+# Copyright 2022 The Uncertainty Baselines Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# pylint: disable=line-too-long
+r"""ViT-B/16 finetuning on a Diabetic Retinopathy Detection dataset.
+
+"""
+# pylint: enable=line-too-long
+
+import ml_collections
+# TODO(dusenberrymw): Open-source remaining imports.
+
+
+def get_sweep(hyper):
+  return hyper.product([])
+
+
+def get_config():
+  """Config for training a patch-transformer on JFT."""
+  config = ml_collections.ConfigDict()
+
+  # Data load / output flags
+
+  # The directory where the model weights and training/evaluation summaries
+  #   are stored.
+  # config.output_dir = (
+      # '/troy/anuj/gub-og/outputs/local')
+  config.output_dir = None
+
+  # Fine-tuning dataset
+  # config.data_dir = '/troy/anuj/gub-mod/uncertainty-baselines/data/downloads/manual/chest_xray'
+  config.data_dir = 'gs://ue-usrl-anuj/data/chest_xray'
+
+  # REQUIRED: distribution shift.
+  # 'aptos': loads APTOS (India) OOD validation and test datasets.
+  #   Kaggle/EyePACS in-domain datasets are unchanged.
+  # 'severity': uses DiabeticRetinopathySeverityShift dataset, a subdivision
+  #   of the Kaggle/EyePACS dataset to hold out clinical severity labels as OOD.
+  config.distribution_shift = 'chxToch14'
+
+  # If checkpoint path is provided, resume training and/or conduct evaluation
+  #   with this checkpoint. See `checkpoint_utils.py`.
+  config.resume = None
+
+  config.prefetch_to_device = 2
+  config.trial = 0
+
+  # Logging and hyperparameter tuning
+
+  config.use_wandb = False  # Use wandb for logging.
+  config.wandb_dir = 'wandb'  # Directory where wandb logs go.
+  config.project = 'ub-debug'  # Wandb project name.
+  config.exp_name = None  # Give experiment a name.
+  config.exp_group = None  # Give experiment a group name.
+
+  # Model Flags
+
+  # TODO(nband): fix issue with sigmoid loss
+  config.num_classes = 5
+
+  # pre-trained model ckpt file
+  # !!!  The below section should be modified per experiment
+  # config.model_init = '/troy/anuj/gub-og/checkpoints/vit_imgnet21k/B_16-i21k-300ep-lr_0.001-aug_medium1-wd_0.1-do_0.0-sd_0.0.npz'
+  # config.model_init = 'gs://ue-usrl-anuj/vit-simclr-outputs/1680700740884/checkpoint_900.npz'
+  # config.model_init = 'gs://vit_models/augreg/B_16-i21k-300ep-lr_0.001-aug_medium1-wd_0.1-do_0.0-sd_0.0.npz'
+  config.model_init = 'gs://vit_models/augreg/B_32-i21k-300ep-lr_0.001-aug_light1-wd_0.1-do_0.0-sd_0.0.npz'
+
+  # Model definition to be copied from the pre-training config
+  config.model = ml_collections.ConfigDict()
+  config.model.patches = ml_collections.ConfigDict()
+  config.model.patches.size = [32, 32]
+  config.model.hidden_size = 768
+  config.model.transformer = ml_collections.ConfigDict()
+  config.model.transformer.attention_dropout_rate = 0.
+  config.model.transformer.dropout_rate = 0.
+  config.model.transformer.mlp_dim = 3072
+  config.model.transformer.num_heads = 12
+  config.model.transformer.num_layers = 12
+  config.model.classifier = 'token'  # Or 'gap'
+
+  # This is "no head" fine-tuning, which we use by default
+  config.model.representation_size = None
+
+  # Preprocessing
+
+  # Input resolution of each retina image. (Default: 256)
+  config.pp_input_res = 128  # pylint: disable=invalid-name
+  pp_common = f''
+  config.pp_train = (
+      f'chest_xray_preprocess({config.pp_input_res})' + pp_common)
+  # 'chest_xray_preprocess(256)|onehot(2)'
+  config.pp_eval = (
+      f'chest_xray_preprocess({config.pp_input_res})' + pp_common)
+
+  # Training Misc
+  config.batch_size = 256  # using TPUv3-8
+  config.seed = 0  # Random seed.
+  config.shuffle_buffer_size = 10_000  # Per host, so small-ish is ok.
+
+  # Optimization
+  config.optim_name = 'Momentum'
+  config.optim = ml_collections.ConfigDict()
+  config.loss = 'sigmoid_xent'  # or 'softmax_xent'
+  config.lr = ml_collections.ConfigDict()
+  config.grad_clip_norm = 1.0  # Gradient clipping threshold.
+  config.weight_decay = None  # No explicit weight decay.
+  config.lr.base = 0.003
+  config.lr.decay_type = 'linear'
+
+  # The dataset is imbalanced (e.g., in Country Shift, we have 19.6%, 18.8%,
+  # 19.2% positive examples in train, val, test respectively).
+  # None (default) will not perform any loss reweighting.
+  # 'constant' will use the train proportions to reweight the binary cross
+  #   entropy loss.
+  # 'minibatch' will use the proportions of each minibatch to reweight the loss.
+  config.class_reweight_mode = 'none'
+
+  # Evaluation Misc
+  config.only_eval = False  # Disables training, only evaluates the model
+  config.eval_on_train = True  # Whether to eval on train split
+  config.use_validation = True  # Whether to use a validation split
+  config.use_test = True  # Whether to use a test split
+
+  # Step Counts
+
+  # Varied together for wandb sweep compatibility.
+  # TODO(nband): revert this to separate arguments.
+  config.total_and_warmup_steps = (34920, 900)
+
+  config.log_training_steps = 100
+  config.log_eval_steps = 1746
+  # NOTE: eval is very fast O(seconds) so it's fine to run it often.
+  config.checkpoint_steps = 1746
+  config.checkpoint_timeout = 1
+
+  config.args = {}
+  return config
+
